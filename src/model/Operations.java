08d9e95e3de1116b5bae346d0174f0f367eadcef
Juan David Cruz Garcia <Angrybirdsgallo@gmail.com>
2020-09-17 16:55:25 -0500

    Añado los respectivos contratos para los metodos y corrijo algunos errores

--- src/model/Operations.java
+++ src/model/Operations.java
@@ -1,39 +1,36 @@
 package model;
 public class Operations{
-	private final static int blackConstructionJob = 1300000;
+	private final static int roughConstruction = 1300000;
 	private final static int painting = 2600000;
-	private final static int whiteConstructionJob = 980000;
+	private final static int finalConstruction = 980000;
+	
+	/**
+	Returns the amount of money that must be paid when the products are bought only in a certain hardware store <br>
+	<b> pre: </b> <br>
+	<b> post: </b> The amount of money to be paid is calculated from the prices of the materials, the quantity, and the location of the residence <br>
+	@param materialPrice Set of prices of a certain hardware store for the materials to be used. materialPrice != null.
+	@param materialQuantity Set of quantities for previously determined types of materials. materialQuantity != null.
+	@param ubication Value that indicates the place of the dwelling, it is a number between 1 and 3. ubication != null.
+	@return total
+	*/
 	public static double totalToPay(double[] materialPrice, double[] materialQuantity,int ubication){
 		double total =0;
 		for(int i=0;i<materialPrice.length;i++){
 			total += materialPrice[i]*materialQuantity[i];
 		}
-		if(total<80000){
-			switch(ubication){
-				case 1:
-				total+=120000;
-				break;
-				case 2:
-				total+=50000;
-				break;
-				case 3:
-				total+=120000;
-				break;
-			}
-		}
-		else if(total<300000){
-			switch(ubication){
-				case 1:
-				total+=28000;
-				break;
-				case 3:
-				total+=55000;
-				break;
-			}
-		}
-		total+=(blackConstructionJob+painting+whiteConstructionJob);
+		total += calculateDeliveryValue(total,ubication);
+		total+=(roughConstruction+painting+finalConstruction);
 		return total;
 	}
+	/**
+	Returns an array of String with the name of the hardware store with the best unit price for a certain material <br>
+	<b> pre: </b> <br>
+	<b> post: </b> The name of the hardware store with the best price for each specific material is found from the price arrays of the 3 possible hardware stores <br>
+	@param HomecenterMaterialPrice Price set from the HomeCenter hardware store for materials to be used. HomeCenterMaterialPrice != Null.
+	@param delCentroMaterialPrice Set of prices of the hardware store of the center for the materials to be used. delCentroMaterialPrice != null.
+	@param delBarrioMaterialPrice Set of prices from the neighborhood hardware store for the materials to be used. delBarrioMaterialPrice != null.
+	@return storeBestPrice
+	*/
 	public static String[] storeBestPrice(double[] homecenterMaterialPrice, double[] delCentroMaterialPrice, double[] delBarrioMaterialPrice){
 		int size = delCentroMaterialPrice.length;
 		String[] storeBestPrice = new String[size];
@@ -55,6 +52,15 @@ public class Operations{
 		}	
 		return storeBestPrice;
 	}
+	/**
+	Returns an array of double with the best unit price for a given material <br>
+	<b> pre: </b> <br>
+	<b> post: </b> The best unit price for a certain material is found from the price arrays of the 3 possible hardware stores <br>
+	@param HomecenterMaterialPrice Price set from the HomeCenter hardware store for materials to be used. HomeCenterMaterialPrice != Null.
+	@param delCentroMaterialPrice Set of prices of the hardware store of the center for the materials to be used. delCentroMaterialPrice != null.
+	@param delBarrioMaterialPrice Set of prices from the neighborhood hardware store for the materials to be used. delBarrioMaterialPrice != null.
+	@return bestPriceValue
+	*/
 	public static double[] bestPriceValue(double[] homecenterMaterialPrice, double[] delCentroMaterialPrice, double[] delBarrioMaterialPrice){
 		int size = homecenterMaterialPrice.length;
 		double[] bestPriceValue = new double[size];
@@ -76,60 +82,69 @@ public class Operations{
 		}	
 		return bestPriceValue;
 	}
-	public static double bestPriceTotal(double[] bestPriceValue, String[] storeBestPrice,int ubication){
+	/**
+	Returns the value of the delivery <br>
+	<b> pre: </b> <br>
+	<b> post: </b> The value of the delivery is found from the total value of all the materials that must be buyed and the ubication of the dwelling <br>
+	@param total  Total value of all the materials that must be buyed
+	@param ubication  Value that indicates the place of the dwelling, it is a number between 1 and 3. ubication != null.
+	@return deliveryValue
+	*/
+	private static double calculateDeliveryValue(double total,int ubication){
+		double deliveryValue = 0;
+		if(total<80000){
+			switch(ubication){
+				case 1:
+					deliveryValue+=120000;
+					break;
+				case 2:
+					deliveryValue+=50000;
+					break;
+				case 3:
+					deliveryValue+=120000;
+					break;
+			}
+		}
+		else if(total<300000){
+			switch(ubication){
+				case 1:
+					deliveryValue+=28000;
+					break;
+				case 3:
+					deliveryValue+=55000;
+					break;
+			}
+		}
+		return deliveryValue;
+	}
+	/**
+	Returns the value that must be paid when the best prices for materials are chosen <br>
+	<b> pre: </b> <br>
+	<b> post: </b> The value is found from the array of best prices, and the ubication of the dwelling <br>
+	@param bestPriceValue Set of the best prices for materials that can be chosen. bestPriceValue != null. 
+	@param ubication  Value that indicates the place of the dwelling, it is a number between 1 and 3. ubication <= 3 && ubication> = 1.
+	param materialQuantity Set of quantities for the materials given. materialQuantity != null.
+	@return total
+	*/
+	public static double bestPriceTotal(double[] bestPriceValue,int ubication,double[] materialQuantity){
 		int size = bestPriceValue.length;
-		double homecenterPrice=0;
-		double delBarrioPrice=0;
-		double delCentroPrice=0;
 		double total=0;
 		for(int i=0;i<size;i++){
-			switch(storeBestPrice[i]){
-				case "Ferreteria del barrio":
-				delBarrioPrice += bestPriceValue[i];
-				break;
-				case "Ferreteria HomeCenter":
-				homecenterPrice += bestPriceValue[i];
-				break;
-				case "Ferreteria del centro":
-				delCentroPrice += bestPriceValue[i];
-				break;
-			}
+			total += bestPriceValue[i]*materialQuantity[i];
 		}
-		double[] prices = new double[3];
-		prices[0] = homecenterPrice;
-		prices[1] = delBarrioPrice;
-		prices[2]= delCentroPrice;
-		for(int i=0;i<3;i++){
-			if(prices[i] != 0){
-				if(prices[i]<80000){
-					switch(ubication){
-						case 1:
-						prices[i]+=120000;
-						break;
-						case 2:
-						prices[i]+=50000;
-						break;
-						case 3:
-						prices[i]+=120000;
-						break;
-					}
-				}
-				else if(total<300000){
-					switch(ubication){
-						case 1:
-						prices[i]+=28000;
-						break;
-						case 3:
-						prices[i]+=55000;
-						break;
-					}
-				}
-			}
-		}
-		total=prices[0]+prices[1]+prices[2];
-		total+= blackConstructionJob+whiteConstructionJob+painting;
+		total += calculateDeliveryValue(total,ubication);
+
 		return total;
 	}
+	/**
+	Returns the size of the set that stores the materials according to their use in the work <br>
+	<b> pre: </b> <br>
+	<b> post: </b> The value of the size of the set is found from the array of the names of materials, the array of the utilization of materials and an indicator for the clasification that is going to be used <br>
+	@param materialName Array of String that stores the names of the materials. materialName != null. 
+	@param materialUtilization  Array of int from 1 to 3 that stores the utilization of the materials. utilization != null
+	@param use Value that indicates for what work is this size reservation going to be made, number 1 means rough construction, number 2 means final construction, number 3 means painting. use >= 1 && use <= 3
+	@return size
+	*/
 	private static int sizeOfString(String[] materialName, int[] materialUtilization,int use){
 		int size =0;
 		for(int i=0; i<materialName.length;i++){
@@ -139,6 +154,15 @@ public class Operations{
 		}
 		return size;
 	}
+	/**
+	Returns a set that stores the materials according to their use in the work <br>
+	<b> pre: </b> <br>
+	<b> post: </b> The set is found from the array of the names of materials, the array of the utilization of materials and an indicator for the clasification that is going to be used <br>
+	@param materialName Array of String that stores the names of the materials. materialName != null. 
+	@param materialUtilization  Array of int from 1 to 3 that stores the utilization of the materials. utilization != null
+	@param use Value that indicates for what work is this size reservation going to be made, number 1 means rough construction, number 2 means final construction, number 3 means painting. use >= 1 && use <= 3
+	@return size
+	*/
 	public static String[] utilization(String[] materialName, int[] materialUtilization, int use){
 		int size = sizeOfString(materialName,materialUtilization,use);
 		int position =0;
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,6 +2,13 @@ package ui;
 import model.*;
 import java.util.Scanner;
 public class Main {
+	/**
+	Read the ubication of the dwelling <br>
+	<b> pre: </b> The user must enter an int number from 1 to 3, 1 means North, 2 means center, 3 means south <br>
+	<b> post: </b> The method returns the value that the user entered
+	@param sc A scanner with the function of reading what the user enters. The scanner must be initialized
+	@return ubication
+	*/
 	public static int askForUbication(Scanner sc){
 		int ubication;
 		System.out.println("Ingrese la ubicacion del inmueble ( 1 para Norte, 2 para Centro, 3 para Sur) ");
@@ -9,6 +16,13 @@ public class Main {
 		sc.nextLine();
 		return ubication;
 	}
+	/**
+	Read the names of the materials <br>
+	<b> pre: </b> The user must enter strings with the names of the materials to be used <br>
+	<b> post: </b> The method returns the values that the user entered
+	@param sc A scanner with the function of reading what the user enters. The scanner must be initialized
+	@return materialName
+	*/
 	public static String[] askForName(Scanner sc){
 		System.out.println("Ingrese cuantos tipos de materiales necesita para la construccion ");
 		int listQuantity= sc.nextInt();
@@ -20,6 +34,14 @@ public class Main {
 		}
 		return materialName;
 	}
+	/**
+	Read the quantities for each type of the materials <br>
+	<b> pre: </b> The user must enter positive double numbers for each type of the materials <br>
+	<b> post: </b> The method returns the values that the user entered
+	@param sc A scanner with the function of reading what the user enters. The scanner must be initialized
+	@param materialName The set of the type of materials that are going to be used. materialName != null.
+	@return materialQuantity
+	*/
 	public static double[] askForQuantity(Scanner sc,String[] materialName){
 		int listQuantity = materialName.length;
 		double[] materialQuantity = new double[listQuantity];
@@ -29,6 +51,14 @@ public class Main {
 		}
 		return materialQuantity;
 	}
+	/**
+	Read for which work will each type of the materials be used <br>
+	<b> pre: </b> The user must enter int numbers from 1 to 3 for each type of the materials. 1 means rough construction, 2 means final construction, 3 means painting  <br>
+	<b> post: </b> The method returns the values that the user entered
+	@param sc A scanner with the function of reading what the user enters. The scanner must be initialized
+	@param materialName The set of the type of materials that are going to be used. materialName != null.
+	@return materialUtilization
+	*/
 	public static int[] askForUtilization(Scanner sc,String[] materialName){
 		int listQuantity = materialName.length;
 		int[] materialUtilization = new int[listQuantity];
@@ -38,6 +68,14 @@ public class Main {
 		}
 		return materialUtilization;
 	}
+	/**
+	Read the prices for the materials of a certain hardware store <br>
+	<b> pre: </b> The user must enter positive double numbers that indicate the amount of materials that will be used for each type of these.<br>
+	<b> post: </b> The method returns the values that the user entered
+	@param sc A scanner with the function of reading what the user enters. The scanner must be initialized
+	@param materialName The set of the type of materials that are going to be used. materialName != null.
+	@return materialPrice
+	*/
 	public static double[] askForPrice(Scanner sc,String[] materialName){
 		int listQuantity=materialName.length;
 		double[] materialPrice = new double[listQuantity];
@@ -47,6 +85,13 @@ public class Main {
 		}
 		return materialPrice;
 	}
+	/**
+	Shows the solution to all the problems that arise in the integrative task <br>
+	<b> pre: </b> All methods must work correctly <br>
+	<b> post: </b> The method shows the solution to all the problems that arise in the integrative task
+	@param sc A scanner with the function of reading what the user enters. The scanner must be initialized
+	@return materialPrice
+	*/
 	public static void showData(Scanner sc){
 		int ubication = askForUbication(sc);
 		String[] materialName= askForName(sc);
@@ -63,13 +108,14 @@ public class Main {
 		String[] blackJobConstructionUtilization = Operations.utilization(materialName,materialUtilization,1);
 		String[] whiteJobConstructionUtilization = Operations.utilization(materialName,materialUtilization,2);
 		String[] paintingUtilization = Operations.utilization(materialName,materialUtilization,3);
+		System.out.println(" ");
 		System.out.println("El total a pagar para HomeCenter es: "+Operations.totalToPay(homeCenterMaterialPrice,materialQuantity,ubication));
-		System.out.println("El total a pagar para HomeCenter es: "+Operations.totalToPay(delCentroMaterialPrice,materialQuantity,ubication));
-		System.out.println("El total a pagar para HomeCenter es: "+Operations.totalToPay(delBarrioMaterialPrice,materialQuantity,ubication));
+		System.out.println("El total a pagar para Ferreteria del centro es: "+Operations.totalToPay(delCentroMaterialPrice,materialQuantity,ubication));
+		System.out.println("El total a pagar para Ferreteria del barrio es: "+Operations.totalToPay(delBarrioMaterialPrice,materialQuantity,ubication));
 		for(int i=0;i<materialName.length;i++){
 			System.out.println("El mejor lugar para comprar "+materialName[i]+" es "+storeBestPrice[i]+" con un valor de "+bestPriceValue[i]);
 		}
-		System.out.println("Si compra con los mejores precios, debera pagar "+Operations.bestPriceTotal(bestPriceValue,storeBestPrice,ubication));
+		System.out.println("Si compra con los mejores precios, debera pagar "+Operations.bestPriceTotal(bestPriceValue,ubication,materialQuantity));
 		System.out.println("Los productos utilizados para la obra negra son: ");
 		for(int i=0;i<blackJobConstructionUtilization.length;i++){
 			System.out.println(blackJobConstructionUtilization[i]);	
@@ -82,8 +128,6 @@ public class Main {
 		for(int i=0;i<paintingUtilization.length;i++){
 			System.out.println(paintingUtilization[i]);	
 		}
-		
-		
 	}
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
